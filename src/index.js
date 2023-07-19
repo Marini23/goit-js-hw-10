7911c5d570772cc0093766771913d440d0f5c0cd
Marini23 <marina.smile1m@gmail.com>
2023-07-19 20:02:07 +0200

    4

--- src/index.js
+++ src/index.js
@@ -1,20 +1,51 @@
-import { fetchBreeds } from "./cat-api";
+import { fetchBreeds, fetchCatByBreed } from "./cat-api";
 
 const select = document.querySelector(`.breed-select`);
 const loader = document.querySelector(`.loader`);
 const error = document.querySelector(`.error`);
-const catInfo = document.querySelector(`.cat-info`);
+const div = document.querySelector(`.cat-info`);
+
+
+
 
 fetchBreeds().then(data => {
+    console.log(`fetchBreeds`)
     select.innerHTML = data.map(element => `<option value="${element.id}">${element.name}</option>`).join(``);
 })
-.catch(() => paragraphError.remoteAttribute(`hiden`))
-.finally(() => paragraphLoading.setAttribute(`hiden`, true))
+.catch(() => error.remoteAttribute(`hidden`))
+.finally(() => loader.setAttribute(`hidden`, true))
 
 
+select.addEventListener(`change`, onChangeBreed);
 
-
-
+function onChangeBreed(e) {
+    // loader.removeAttribute(`hidden`);
+    let breedId = e.target.value;
+    // console.log(breedId);
+    fetchCatByBreed(breedId)
+        .then(data => {
+            console.log(`fetchCatByBreed`)
+            console.log(data);
+            div.innerHTML = data.map(element =>
+                `<div><img src="${element.url}" alt="photo cat" width="500" height="400"/></div>`).join(``)
+            data.map(el => el.breeds.forEach(cat => {
+                const array = [cat];
+                const findCat = array.find(option => option.id === breedId);
+                // console.log(findCat.description);
+                const makrup = `<h2>${findCat.name}</h2>
+                <p>${findCat.description}</p>
+                <h3>Temperament</h3>
+                <p>${findCat.temperament}</p>`
+                div.insertAdjacentHTML(`beforeend`, makrup)
+            }))
+        })
+        .catch(() => {
+        error.removeAttribute(`hidden`)
+        })
+        .finally(() => {
+        loader.setAttribute(`hidden`,true)
+    })
+}
 
 
 
