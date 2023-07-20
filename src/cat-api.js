341c08f8faf4ec9a6487d5d6266e96ec89dec55b
Marini23 <marina.smile1m@gmail.com>
2023-07-20 18:35:49 +0200

    6-fix

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,50 +1,26 @@
 import axios from "axios";
-// axios.defaults.headers.common[`x-api-key`] = `live_78i5BMDbEVtvm4bF6CtHppjOH5FDijMJq91f3amTjvcMULPghgj1K1AAmwWlV7xq`;
-// axios.defaults.baseURL = 'https://api.thecatapi.com/v1/';
-
-// export function fetchBreeds() {
-//     axios.get(`/breeds`).then(response => {
-//         if (response.status !== 200) {
-//             throw new Error(response.status)
-//         }
-//         return response.data;
-//     })
-// }
-
-// export function fetchCatByBreed(breedId) {
-//     axios.get(`/images/search?breed_ids={breed.id}`)
-//     .then((response) => {
-//         if (!response.ok) {
-//         throw new Error(response.statusText)
-//     }
-// return response.data;
-// })
-// }
-// -----------
-
-const options = {headers: {
-    'x-api-key': `live_78i5BMDbEVtvm4bF6CtHppjOH5FDijMJq91f3amTjvcMULPghgj1K1AAmwWlV7xq`
-}
-}
+axios.defaults.headers.common[`x-api-key`] = `live_78i5BMDbEVtvm4bF6CtHppjOH5FDijMJq91f3amTjvcMULPghgj1K1AAmwWlV7xq`;
+axios.defaults.baseURL = 'https://api.thecatapi.com/v1/';
 
 export function fetchBreeds() {
-return fetch(`https://api.thecatapi.com/v1/breeds`, options)
-    .then((response) => {
-        if (!response.ok) {
-        throw new Error(response.statusText)
-    }
-return response.json();
-})}
+    return axios.get(`/breeds`).then(response => {
+        if (response.status !== 200) {
+            throw new Error(response.status)
+        }
+        return response.data;
+    })
+}
 
 export function fetchCatByBreed(breedId) {
-    return fetch(`https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`, options)
-    .then((response) => {
-        if (!response.ok) {
+    return axios.get(`/images/search?breed_ids=${breedId}`)
+        .then((response) => {
+        if (response.status !== 200) {
         throw new Error(response.statusText)
     }
-return response.json();
+    return response.data;
 })
 }
+
 
 
 
--- src/index.js
+++ src/index.js
@@ -5,13 +5,17 @@ const loader = document.querySelector(`.loader`);
 const error = document.querySelector(`.error`);
 const div = document.querySelector(`.cat-info`);
 
-error.style.visibility = `hidden`;
-select.style.visibility = `hidden`;
 
+
+
+// -----
+select.style.visibility = `hidden`;
+error.style.visibility = `hidden`;
 
 fetchBreeds().then(data => {
     loader.style.visibility = `visible`;
-    // console.log(`fetchBreeds`)
+    // error.style.visibility = `hidden`;
+    console.log(`fetchBreeds`)
     loader.style.visibility = `hidden`;
     select.style.visibility = `visible`;
     select.innerHTML = data.map(element => `<option value="${element.id}">${element.name}</option>`).join(``);
@@ -24,35 +28,31 @@ select.addEventListener(`change`, onChangeBreed);
 
 function onChangeBreed(e) {
     e.preventDefault();
+    let breedId = e.target.value;
+    console.log(breedId);
     loader.style.visibility = `visible`;
     div.style.visibility = `hidden`;
-    let breedId = e.target.value;
-    // console.log(breedId);
     fetchCatByBreed(breedId)
         .then(data => {
-            // console.log(`fetchCatByBreed`)
+            console.log(data);
             loader.style.visibility = `hidden`;
             div.style.visibility = `visible`;
-            // console.log(data);
+            console.log(data);
             div.innerHTML = data.map(element =>
                 `<div><img src="${element.url}" alt="photo cat" width="500" height="400"/></div>`).join(``)
             data.map(el => el.breeds.forEach(cat => {
                 const array = [cat];
                 const findCat = array.find(option => option.id === breedId);
-                // console.log(findCat.description);
-                const makrup = `<div>
+                console.log(findCat.description);
+                const makrup = `<div class="cat-card">
                 <h2>${findCat.name}</h2>
-                <p>${findCat.description}</p>
+                <p class="p-description">${findCat.description}</p>
                 <h3>Temperament</h3>
                 <p>${findCat.temperament}</p>
                 </div>`
                 div.insertAdjacentHTML(`beforeend`, makrup)
             }))
         })
-        .catch((error) => error.style.visibility = `visible`)
+        .catch(() => error.style.visibility = `visible`)
         .finally(() => loader.style.visibility = `hidden`)
-}
-
-
-
-
+};
